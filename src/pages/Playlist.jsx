5fc9c52a8532e1331edea7001daf043ffa4b8847
Sachin Soni <your_email@example.com>
2026-01-10 21:26:39 +0530

    Restore Playlist page with Navbar, Playlist component, MusicPlayer and Footer

--- src/pages/Playlist.jsx
+++ src/pages/Playlist.jsx
@@ -1,9 +1,23 @@
-function Playlist() {
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import MusicPlayer from '../components/MusicPlayer'
+import Playlist from '../components/Playlist'
+
+function PlaylistPage() {
   return (
-    <div style={{ color: "white", padding: "40px" }}>
-      Playlist page working ✅
-    </div>
-  );
+    <>
+      <Navbar />
+      <main className="page-container">
+        <section className="section-container">
+          <h1 style={{ color: 'white', marginBottom: 16 }}>Playlists</h1>
+          <Playlist />
+        </section>
+      </main>
+
+      <MusicPlayer />
+      <Footer />
+    </>
+  )
 }
 
-export default Playlist;
+export default PlaylistPage;
