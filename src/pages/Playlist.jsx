c8988e62f6b7ac99132088620726d64521e942a2
Sachin Soni <your_email@example.com>
2026-01-10 21:39:28 +0530

    Render PlaylistSection on /playlists page (include Navbar & Footer)

--- src/pages/Playlist.jsx
+++ src/pages/Playlist.jsx
@@ -1,23 +1,17 @@
 import Navbar from '../components/Navbar'
+import PlaylistSection from '../components/PlaylistSection'
 import Footer from '../components/Footer'
-import MusicPlayer from '../components/MusicPlayer'
-import Playlist from '../components/Playlist'
 
-function PlaylistPage() {
+function Playlist() {
   return (
     <>
       <Navbar />
-      <main className="page-container">
-        <section className="section-container">
-          <h1 style={{ color: 'white', marginBottom: 16 }}>Playlists</h1>
-          <Playlist />
-        </section>
+      <main style={{ padding: '40px 20px', minHeight: '60vh' }}>
+        <PlaylistSection />
       </main>
-
-      <MusicPlayer />
       <Footer />
     </>
   )
 }
 
-export default PlaylistPage;
+export default Playlist;
